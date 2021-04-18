02a9f5ce1f13d9bf74f4d483eca80ac6d7bb3295
thedejaware <mehmet.akin84@gmail.com>
2021-04-18 17:08:16 +0300

    - Docker compose is created with Container Orchestrator Support - Portainer, Rig.API and MSSQL Images have been pulled and continerized

---
+++ src/Services/API/RIG.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RIG.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet(Name = "Login")]
+        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<object>> Loginn()
+        {
+            var arr = new string[] { "1", "2" };
+            return Ok(arr);
+        }
+    }
+}
